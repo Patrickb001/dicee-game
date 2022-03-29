77d69d41ead0b9f7f1c9cae0af21bfbf32f777f6
PatrickB001 <patrickborgella1@gmail.com>
2022-03-29 14:53:17 -0500

    Adds functionality, does not start the game until the user refreshes the window

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const image1 = document.querySelector(".img1");
-const image2 = document.querySelector(".img2");
+const image1 = document.querySelectorAll("img")[0];
+const image2 = document.querySelectorAll("img")[1];
 const h1 = document.querySelector("h1");
 
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+if (performance.getEntriesByType("navigation")[0].type === "reload") {
+  rollDice(randomNumber1, randomNumber2);
+}
+
 function rollDice(die1, die2) {
+  if (randomNumber1 > randomNumber2) h1.innerHTML = "Player 1 Wins! 🚩";
+  else if (randomNumber1 < randomNumber2) h1.innerHTML = "Player 2 Wins! 🚩";
+  else h1.textContent = "Draw!";
   return {
     image1: image1.setAttribute("src", `images/dice${die1}.png`),
     image2: image2.setAttribute("src", `images/dice${die2}.png`),
   };
 }
-
-rollDice(randomNumber1, randomNumber2);
-
-if (randomNumber1 > randomNumber2) h1.textContent = "Player 1 Wins! 🚩";
-else if (randomNumber1 < randomNumber2) h1.textContent = "Player 2 Wins! 🚩";
-else h1.textContent = "Draw!";
