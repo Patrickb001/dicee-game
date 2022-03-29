c7f1b2ccbdf8f72274688b58baad1c959b05aefd
PatrickB001 <patrickborgella1@gmail.com>
2022-03-29 14:00:25 -0500

    Initial commit, adds script.js, dicee.html, and styles.css. Initial game complete

---
+++ script.js
@@ -0,0 +1,22 @@
+const image1 = document.querySelector(".img1");
+const image2 = document.querySelector(".img2");
+const h1 = document.querySelector("h1");
+
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+// image1.setAttribute("src", `images/dice${randomNumber1}.png`);
+// image2.setAttribute("src", `images/dice${randomNumber2}.png`);
+
+function rollDice(die1, die2) {
+  return {
+    image1: image1.setAttribute("src", `images/dice${die1}.png`),
+    image2: image2.setAttribute("src", `images/dice${die2}.png`),
+  };
+}
+
+const die = rollDice(randomNumber1, randomNumber2);
+
+if (randomNumber1 > randomNumber2) h1.textContent = "Player 1 Wins! 🚩";
+else if (randomNumber1 < randomNumber2) h1.textContent = "Player 2 Wins! 🚩";
+else h1.textContent = "Draw!";
